108ed7864ad9c89aaacff633913dd28909d32175
FernandoRogelin <nando_rogelin@hotmail.com>
2018-11-04 19:33:04 -0200

    Menu para selecionar quantos pedidos quer

--- src/components/Quantity/index.js
+++ src/components/Quantity/index.js
@@ -3,21 +3,18 @@ import PropTypes from 'prop-types';
 
 import { Wrapper, Select, Option } from './styles';
 
-export default function Quantity ({...props}) {
+export default function Quantity ({value, ...props}) {
   return(
     <Wrapper {...props}>
       <Select name="quantidade">
-        <Option value="0">0</Option>
-        <Option value="1">1</Option>
-        <Option value="2">2</Option>
-        <Option value="3">3</Option>
-        <Option value="4">4</Option>
-        <Option value="5">5</Option>
+        {value.map(value => {
+          return <Option key={value} value={value}>{value}</Option>
+        })}
       </Select>
     </Wrapper>
   );
 }
 
 Quantity.propTypes = {
-  children: PropTypes.number.isRequired,
+  value: PropTypes.array.isRequired,
 }
--- src/containers/Hamburguer/index.js
+++ src/containers/Hamburguer/index.js
@@ -10,16 +10,25 @@ import { Content, Image } from './styles';
 import fotoHamburguer from '@/Images/background.jpg'
 
 export default class Hamburguer extends Component {
+  state = {
+    value: [0,1,2,3,4,5],
+  }
+
+  selectIngredients = () => {
+    console.log('clicando');
+  };
+
   render() {
     return (
       <Container>
         <Wrapper>
           <Head title="Hamburguer" />
           <Content>
-            <Image src={fotoHamburguer} alt="Foto Hamburguer" />
-            <Description>asuhuhasuhasu</Description>
+            <Image onClick={this.selectIngredients} src={fotoHamburguer} alt="Foto Hamburguer" />
+            <Description>Nós possuímos uma ótima promoção para quem quer comer um delicioso hamburguer com aquela coca bem gelada.
+          <br /><br /> Ingredientes: Pão, Carne, Bacon, Alface, Tomate.</Description>
           </Content>
-          <Quantity />
+          <Quantity value={this.state.value} />
         </Wrapper>
       </Container>
     );
